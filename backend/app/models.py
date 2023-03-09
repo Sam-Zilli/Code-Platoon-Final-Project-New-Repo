75664946667c430187a05f7f30b64ed4bde5be8a
Sam Z <samzilli01@gmail.com>
2023-03-09 18:56:22 -0500

    added tours to database

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
        
     image = models.ImageField(null=True, blank=True,
-                              default='/placeholder.png') 
+                              default='/ts.jpg') 
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    value = models.IntegerField(default=0)
+    value = models.IntegerField()
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
