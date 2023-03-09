f62810b9928f84c940acf2e24d54899054b0832a
Sam Z <samzilli01@gmail.com>
2023-03-09 16:49:48 -0500

    got rid of profile dropdown in navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -43,9 +43,9 @@ function Header() {
 
           { userInfo ? (
             <NavDropdown title={userInfo.name} id='username'>
-              <LinkContainer to='/profile'>
+              {/* <LinkContainer to='/profile'>
                 <NavDropdown.Item>Profile</NavDropdown.Item>
-              </LinkContainer>
+              </LinkContainer> */}
 
               <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
 
